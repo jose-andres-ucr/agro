2dbd8648d5e5a5a2fb44a7625f2bab366d72cc58
Randy <randy.robles@ucr.ac.cr>
2024-05-10 01:40:43 -0600

    Consultar comentarios

--- my-app/app/components/CommentLog.tsx
+++ my-app/app/components/CommentLog.tsx
@@ -1,6 +1,7 @@
 import { Button, Text, View, StyleSheet } from "react-native";
 import React, { useEffect, useState } from 'react';
 import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
+import { ScrollView } from 'react-native';
 
 
 type Comment = {
@@ -24,40 +25,82 @@ export const CommentLog = (props: { text: string }) => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Comentarios</Text>
-      <View style={styles.separator} />
-      {showComments && (
-        <View>
-          {comments.map((comment, index) => (
-            <View style={styles.container} key ={index}>
-              <Text>Nombre: {comment.Name}</Text>
-              <Text>Fecha y hora: {new Date(comment.DateTime.toDate()).toLocaleString()}</Text>
-              <Text>Comentario: {comment.Comment}</Text>
-            </View>
-          ))}
+    <ScrollView
+    contentContainerStyle={{ flexGrow: 1 }}
+    ref={(scrollView) => { scrollView?.scrollToEnd({ animated: true }); }}
+    >
+      <View style={styles.container}>
+        <View style={{alignItems: 'flex-start'}}>
+        <Button
+          title={showComments ? "Ocultar comentarios" : "Mostrar comentarios"}
+          onPress={() => setShowComments(!showComments)}
+        />
         </View>
-      )}
-      <Button
-        title={showComments ? "Ocultar comentarios" : "Mostrar comentarios"}
-        onPress={() => setShowComments(!showComments)}
-      />
-    </View>
+        {showComments && (
+          <View>
+            <View style={styles.separator} />
+            <Text style={styles.title}>Comentarios</Text>
+            {comments.map((comment, index) => (
+              <View>
+              {comment.Name && comment.DateTime && comment.Comment ? (
+                <>
+                  <View style={styles.commentBox} key={index}>
+                    <View style={styles.commentContainer}>
+                      <Text style={styles.commentName}>Nombre: {comment.Name}</Text>
+                      <Text style={styles.commentDateTime}>Fecha y hora: {new Date(comment.DateTime.toDate()).toLocaleString()}</Text>
+                      <Text style={styles.commentText}>Comentario: {comment.Comment}</Text>
+                    </View>
+                  </View>
+                </>
+              ) : null}
+              </View>
+            ))}
+          </View>
+        )}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+
     justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'steelblue',
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
-    width: '80%',
+    width: '100%',
+    backgroundColor: 'steelblue',
+  },
+  commentBox: {
+    marginBottom: 10,
+    borderWidth: 3,
+    borderRadius: 15,
+    borderColor: 'lightsteelblue',
+    alignSelf: 'stretch',
+  },
+  commentContainer: {
+    padding: 10,
+  },
+  commentName: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  commentDateTime: {
+    fontStyle: 'italic',
+    marginBottom: 5,
+    color: 'gray',
+  },
+  commentText: {
+    lineHeight: 20,
   },
 });
