032df93a05c6ee7003cb59d22ea0b4856fd2e506
BASM7 <j.alonsomm07@gmail.com>
2024-05-30 01:01:40 -0600

    issue_37
    added toast errors and refactored code

--- my-app/app/components/calculators/PesticidePerPlant.tsx
+++ my-app/app/components/calculators/PesticidePerPlant.tsx
@@ -5,72 +5,60 @@ import { z } from "zod";
 import React, { useEffect, useState } from "react";
 import { CommentLog } from "./CommentLog";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Toast from "react-native-toast-message";
-import useGlobalCalculatorStyles from "@/constants/styles";
+import { showToastError } from "@/constants/utils";
+import useGlobalstyles from "@/constants/styles";
 
 
 const schema = z.object({
   plantCuantity: z
-    .string()
-    .min(1, { message: "Este campo es obligatorio" })
+    .string({required_error: "Este campo es obligatorio"})
     .refine((val) => !isNaN(Number(val)), { message: "Debe ser un valor numérico" })
-    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" }),
-    initialVolume: z
-    .string()
-    .min(1, { message: "Este campo es obligatorio" })
+    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" })
+    .transform((val) => Number(val)),
+
+  initialVolume: z
+    .string({required_error: "Este campo es obligatorio"})    
     .refine((val) => !isNaN(Number(val)), { message: "Debe ser un valor numérico" })
-    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" }),
-    finalVolume: z
-    .string()
-    .min(1, { message: "Este campo es obligatorio" })
+    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" })
+    .transform((val) => Number(val)),
+
+  finalVolume: z
+    .string({required_error: "Este campo es obligatorio"})    
     .refine((val) => !isNaN(Number(val)), { message: "Debe ser un valor numérico" })
-    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" }),
-    plantCuantityTotal: z
-    .string()
-    .min(1, { message: "Este campo es obligatorio" })
+    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" })
+    .transform((val) => Number(val)),
+
+  plantCuantityTotal: z
+    .string({required_error: "Este campo es obligatorio"})    
     .refine((val) => !isNaN(Number(val)), { message: "Debe ser un valor numérico" })
-    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" }),
+    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, { message: "Debe ser un número positivo" })
+    .transform((val) => Number(val)),
 });
 
 type FormData = z.infer<typeof schema>;
 
 export default function PesticidePerPlant() {
-  const calculatorStyles = useGlobalCalculatorStyles();
+  const styles = useGlobalstyles();
   const [result, setResult] = useState<string | null>(null);
-  const showToast = (title:string, message: string | undefined) => {
-    Toast.show({
-      type: "error",
-      text1: title,
-      text2: message || "Error",
-      position: "bottom",
-      visibilityTime: 3000,
-    });
-  }  
 
   const {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>({
-    defaultValues: {
-      plantCuantity: "",
-      initialVolume: "",
-      finalVolume: "",
-      plantCuantityTotal: "",
-    },
+  } = useForm<FormData>({    
     resolver: zodResolver(schema),
   });
 
   useEffect(() => {
     if (errors) {
       if (errors.plantCuantity) {
-        showToast("Cantidad de plantas aplicadas", errors.plantCuantity.message);
+        showToastError("Cantidad de plantas aplicadas", errors.plantCuantity.message);
       } else if (errors.initialVolume) {
-        showToast("Volumen inicial", errors.initialVolume.message);
+        showToastError("Volumen inicial", errors.initialVolume.message);
       } else if (errors.finalVolume) {
-        showToast("Volumen final", errors.finalVolume.message);
+        showToastError("Volumen final", errors.finalVolume.message);
       } else if (errors.plantCuantityTotal) {
-        showToast("Plantas por aplicar", errors.plantCuantityTotal.message);
+        showToastError("Plantas por aplicar", errors.plantCuantityTotal.message);
       }
     }
   }, [errors]);
@@ -94,21 +82,21 @@ export default function PesticidePerPlant() {
       plantCuantityTotal,
     } = data;
 
-    const result = ((Number(initialVolume) - Number(finalVolume)) * Number(plantCuantityTotal)) / Number(plantCuantity);
+    let result = ((initialVolume - finalVolume) * plantCuantityTotal) / plantCuantity;
     setResult(result.toFixed(3));
   };
 
   return (
     <ScrollView
-    contentContainerStyle={calculatorStyles.scrollView}
+    contentContainerStyle={styles.scrollView}
     ref={(scrollView) => { scrollView?.scrollToEnd({ animated: true }); }}
     >
-      <View style={calculatorStyles.mainContainer}>
-        <Text style={calculatorStyles.header}>Calibración por planta</Text>
-        <Text style={ calculatorStyles.body }>Cuente un número de plantas y aplique allí agua a la velocidad usual.</Text>
+      <View style={styles.mainContainer}>
+        <Text style={styles.header}>Calibración por planta</Text>
+        <Text style={ styles.body }>Cuente un número de plantas y aplique allí agua a la velocidad usual.</Text>
         
-        <View style={calculatorStyles.formContainer}>
-          <View style={calculatorStyles.inputGroup}>
+        <View style={styles.formContainer}>
+          <View style={styles.inputGroup}>
             <Controller
               control={control}
               render={({ field: { onChange, onBlur, value } }) => (
@@ -116,7 +104,7 @@ export default function PesticidePerPlant() {
                   ref={refs.plantCuantityRef}
                   label="Cantidad de plantas aplicadas"
                   mode="outlined"
-                  style={calculatorStyles.inputField}
+                  style={styles.inputField}
                   onBlur={onBlur}
                   onChangeText={onChange}
                   value={value?.toString()}                  
@@ -134,7 +122,7 @@ export default function PesticidePerPlant() {
             />
           </View>
 
-          <View style={calculatorStyles.inputGroup}>          
+          <View style={styles.inputGroup}>          
             <Controller
               control={control}
               render={({ field: { onChange, onBlur, value } }) => (
@@ -142,7 +130,7 @@ export default function PesticidePerPlant() {
                   ref={refs.initialVolumeRef}
                   label="Volumen inicial"
                   mode="outlined"
-                  style={calculatorStyles.inputField}
+                  style={styles.inputField}
                   onBlur={onBlur}
                   onChangeText={onChange}
                   value={value?.toString()}                  
@@ -157,10 +145,10 @@ export default function PesticidePerPlant() {
               )}
               name="initialVolume"
             />
-            <Text style={calculatorStyles.text}>Litros</Text>
+            <Text style={styles.text}>Litros</Text>
           </View>
 
-          <View style={calculatorStyles.inputGroup}>          
+          <View style={styles.inputGroup}>          
             <Controller
               control={control}
               render={({ field: { onChange, onBlur, value } }) => (
@@ -168,7 +156,7 @@ export default function PesticidePerPlant() {
                   ref={refs.finalVolumeRef}
                   label="Volumen final"
                   mode="outlined"
-                  style={calculatorStyles.inputField}
+                  style={styles.inputField}
                   onBlur={onBlur}
                   onChangeText={onChange}
                   value={value?.toString()}                  
@@ -183,10 +171,10 @@ export default function PesticidePerPlant() {
               )}
               name="finalVolume"
             />
-            <Text style={calculatorStyles.text}>Litros</Text>
+            <Text style={styles.text}>Litros</Text>
           </View>
           
-          <View style={calculatorStyles.inputGroup}>
+          <View style={styles.inputGroup}>
             <Controller
               control={control}
               render={({ field: { onChange, onBlur, value } }) => (
@@ -194,7 +182,7 @@ export default function PesticidePerPlant() {
                   ref={refs.cuantityTotal}
                   label="Cantidad de plantas por aplicar"
                   mode="outlined"
-                  style={calculatorStyles.inputField}
+                  style={styles.inputField}
                   onBlur={onBlur}
                   onChangeText={onChange}
                   value={value?.toString()}
@@ -207,25 +195,25 @@ export default function PesticidePerPlant() {
               )}
               name="plantCuantityTotal"
             />
-            <Text style={calculatorStyles.text}>m2</Text>
+            <Text style={styles.text}>m2</Text>
           </View>
         </View>        
 
         <Button
-          style={calculatorStyles.button}
+          style={styles.button}
           mode="contained"
           onPress={handleSubmit(onSubmit)}
         >
           Calcular
         </Button>
 
-        <View style={calculatorStyles.resultGroup}>
+        <View style={styles.resultGroup}>
           <TextInput
-            style={calculatorStyles.resultField}
+            style={styles.resultField}
             value={result?.toString()}
             editable={false}
           />
-          <Text style={calculatorStyles.text}> Litros</Text>
+          <Text style={styles.text}> Litros</Text>
         </View>
       </View>
       <CommentLog text="PesticidePerPlantComments" />
