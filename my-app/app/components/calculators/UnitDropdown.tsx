1d806d505c9df3781fef940a1425f1f98dee8944
BASM7 <j.alonsomm07@gmail.com>
2024-06-03 16:01:51 -0600

    issue_34
    create new components to display units.

--- my-app/app/components/calculators/UnitDropdown.tsx
+++ my-app/app/components/calculators/UnitDropdown.tsx
@@ -3,30 +3,35 @@ import React, { useState } from 'react';
 import { Dropdown } from 'react-native-element-dropdown';
 import { Unit } from '@/constants/units';
 
-export const DropdownComponent = (props: { data: Unit[] }) => {
+
+export const DropdownComponent = (props: { data: Unit[], isModal: boolean, value: string | null, onValueChange: (value: string) => void }) => {
     const styles = useGlobalDropdownStyles();
-    const [value, setValue] = useState<string | null>(props?.data[0].value);
+    const [value, setValue] = useState<string | null>(props.value);
     const [isFocus, setIsFocus] = useState(false);
+
+    const onChange = (item: Unit) => {
+        setValue(item.value);
+        setIsFocus(false);
+        props.onValueChange(item.value);
+    }
 
     return (
         <Dropdown
-            style={isFocus ? styles.focusedDropdown : styles.dropdown}
-            placeholderStyle={styles.placeholderStyle}
-            
+            style={isFocus ? styles.focusedDropdown : styles.dropdown}  
+            containerStyle={styles.dropdownItem}                      
             selectedTextStyle={styles.selectedTextStyle}
+            activeColor={styles.dropdown.backgroundColor}
+            itemTextStyle={styles.itemTextStyle}
             data={props.data}
-            mode="modal"           
-            maxHeight={300}
+            mode={props.isModal ? 'modal' : 'default'}
+            maxHeight={300}            
             labelField="value"
             valueField="value"
             placeholder={value || ''}
             value={value}
             onFocus={() => setIsFocus(true)}
             onBlur={() => setIsFocus(false)}
-            onChange={item => {
-            setValue(item.value);
-            setIsFocus(false);
-            }}
+            onChange={onChange}
         />
     );
 };
---
+++ my-app/app/components/calculators/UnitModal.tsx
@@ -0,0 +1,59 @@
+import useGlobalDropdownStyles from '@/constants/GlobalDropdownStyle';
+import { useState } from 'react';
+import { Modal, Portal, Text, Button } from 'react-native-paper';
+import { DropdownComponent } from './UnitDropdown';
+import { Unit } from '@/constants/units';
+import { View } from 'react-native';
+
+
+export const UnitModal = (props: {leftUnits: Unit[], rightUnits: Unit[], leftValue: string, rightValue: string, onLeftUnitChange: (value: string) => void, onRightUnitChange: (value: string) => void}) => {
+    const styles = useGlobalDropdownStyles();
+    const [visible, setVisible] = useState(false);
+
+    const [leftUnitValue, setLeftUnitValue] = useState<string>(props.leftValue);
+    const [rightUnitValue, setRightUnitValue] = useState<string>(props.rightValue);
+
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+
+    const handleLeftUnitChange = (value: string) => {
+        setLeftUnitValue(value);
+        props.onLeftUnitChange(value);
+    }
+
+    const handleRightUnitChange = (value: string) => {
+        setRightUnitValue(value);
+        props.onRightUnitChange(value);
+    }
+
+    return (
+        <>
+            <Portal>
+                <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.modalContainer}>                    
+                    <View style={styles.dropDownRowContainer}>
+                        <DropdownComponent
+                        data={props.leftUnits}
+                        isModal={false}
+                        value={leftUnitValue}
+                        onValueChange={handleLeftUnitChange}>              
+                        </DropdownComponent>
+                        <Text style={styles.text}>/</Text>
+                        <DropdownComponent
+                        data={props.rightUnits}
+                        isModal={false}
+                        value={rightUnitValue}
+                        onValueChange={handleRightUnitChange}>              
+                        </DropdownComponent>
+                    </View>
+                </Modal>
+            </Portal>
+            <Button 
+            mode="contained"
+            style={styles.multiUnitButton}
+            labelStyle={styles.multiUnitButtonText}
+            onPress={showModal}>
+            {leftUnitValue + '/' + rightUnitValue}
+            </Button>
+        </>
+    );
+};
