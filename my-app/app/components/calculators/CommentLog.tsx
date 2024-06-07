70b559283b93b447ac0e2734f99494ae28f51722
Randy <randy.robles@ucr.ac.cr>
2024-06-07 17:25:28 -0600

    Se agrega comentarios multimedia.

--- my-app/app/components/calculators/CommentLog.tsx
+++ my-app/app/components/calculators/CommentLog.tsx
@@ -1,11 +1,20 @@
-import { Button, Text, View, StyleSheet, ScrollView, TextInput, ActivityIndicator } from "react-native";
+import { Button, Text, View, StyleSheet, ScrollView, TextInput, ActivityIndicator, TouchableOpacity, Linking } from "react-native";
 import React, { useEffect, useState } from 'react';
 import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
 import { Controller, useForm } from 'react-hook-form';
 import { useFetchUserData } from "../../hooks/FetchData";
 import auth from "@react-native-firebase/auth";
+import * as DocumentPicker  from 'expo-document-picker';
+import storage from '@react-native-firebase/storage';
+import { Image } from 'react-native';
+import Video, {VideoRef} from 'react-native-video';
+import styles from '../styles';
+import Sound from 'react-native-sound';
+
 
 type Comment = {
+  id: string;
+  Attachment: string[];
   Name: string;
   DateTime: FirebaseFirestoreTypes.Timestamp;
   Comment: string;
@@ -16,7 +25,12 @@ export const CommentLog = (props: { text: string }) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { control, handleSubmit, reset } = useForm();
   const { userAuth, userData } = useFetchUserData();
-
+  const [fileUris, setFileUris] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isPaused] = useState(true);
+  const [audio, setAudio] = useState<Sound | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  
   useEffect(() => {
     const subscriber = firestore()
       .collection(props.text)
@@ -30,21 +44,97 @@ export const CommentLog = (props: { text: string }) => {
     return () => subscriber();
   }, []);
 
+  const pickFile = async () => {
+    try {
+      const documentResult = await DocumentPicker.getDocumentAsync();
+      const urifile = documentResult.assets![0];
+
+
+        const fileUri = urifile.uri;
+        const fileName = urifile.name;
+        setFileUris(prevFileUris => [...prevFileUris, fileUri]);
+
+
+    } catch (err) {
+      console.error("Error picking file: ", err);
+    }
+  };
+
+  const uploadFiles = async (uris: string[], postId: string) => {
+    try {
+      const uploadTasks = uris.map(async (uri) => {
+        const filename = uri.substring(uri.lastIndexOf('/') + 1);
+        const uploadUri = uri.replace('file://', '');
+        const task = storage().ref(filename).putFile(uploadUri);
+        console.log(uploadUri);
+        await task;
+        return storage().ref(filename).getDownloadURL();
+      });
+
+      const urls = await Promise.all(uploadTasks);
+
+      await firestore().collection(props.text).doc(postId).update({
+        Attachment: urls,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   const addComment = async (data: any) => {
     setIsSubmitting(true);
     try {
-      await firestore().collection(props.text).add({
+      const postRef = await firestore().collection(props.text).add({
+        Attachment: [],
         Name: data.Name,
         DateTime: firestore.Timestamp.fromDate(new Date()),
         Comment: data.Comment
       });
       reset();
+
+      if (fileUris.length > 0) {
+        await uploadFiles(fileUris, postRef.id);
+        setFileUris([]);
+      }
+
     } catch (error) {
       console.error("Error agregando el comentario: ", error);
     } finally {
       setIsSubmitting(false);
     }
   };
+
+  const playAudio = (uri: string) => {
+    if (audio) {
+      audio.stop(() => {
+        audio.release();
+      });
+    }
+  
+    const sound = new Sound(uri, Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      setAudio(sound);
+      sound.play(() => {
+        sound.release();
+        setIsPlaying(false);
+      });
+    });
+    setIsPlaying(true);
+  };
+  
+  const stopAudio = () => {
+    if (audio) {
+      audio.stop(() => {
+        audio.release();
+        setAudio(null);
+      });
+      setIsPlaying(false);
+    }
+  };
+  
 
   return (
     <ScrollView
@@ -91,22 +181,71 @@ export const CommentLog = (props: { text: string }) => {
                   )}
                   name="Comment"
                 />
-                {isSubmitting ? (
-                  <ActivityIndicator size="large" color="steelblue" />
+                <Button title="Elegir Archivo" onPress={pickFile} />
+                {fileUris && <Text>Archivo seleccionado: {fileUris.join(', ')}</Text>}
+                {loading ? (
+                  <ActivityIndicator size="large" color="#0000ff" />
                 ) : (
                   <Button onPress={handleSubmit(addComment)} title="Enviar" />
                 )}
+                {isSubmitting && <ActivityIndicator size="large" color="steelblue" />}
               </View>
             </>
           )}
           <Text style={styles.title}>Comentarios</Text>
           {comments.map((comment, index) => (
             <View key={index}>
-              <View style={styles.commentBox} key={index}>
-                <View style={styles.commentContainer}>
-                  <Text style={styles.commentName}>{comment.Name}</Text>
-                  <Text style={styles.commentDateTime}>{new Date(comment.DateTime.toDate()).toLocaleString()}</Text>
-                  <Text style={styles.commentText}>{comment.Comment}</Text>
+              <View style={stylesLocal.commentBox} key={index}>
+                <View style={stylesLocal.commentContainer}>
+                  <Text style={stylesLocal.commentName}>{comment.Name}</Text>
+                  <Text style={stylesLocal.commentDateTime}>{new Date(comment.DateTime.toDate()).toLocaleString()}</Text>
+                  <Text style={stylesLocal.commentText}>{comment.Comment}</Text>
+                  {Array.isArray(comment.Attachment) && comment.Attachment.map((attachment, attachmentIndex) => (
+                    <View key={attachmentIndex} style={{ maxWidth: '100%', marginBottom: 10 }}>
+                      {typeof attachment === 'string' && attachment.startsWith('http') ? (
+                        attachment.includes('.jpg') ? (
+                          <Image 
+                            source={{ uri: attachment }} 
+                            style={{ width: '100%', aspectRatio: 1, marginBottom: 10 }} 
+                            resizeMode="contain"
+                          />
+                        ) : attachment.includes('.mp4') ? (
+                          <View style={{ maxWidth: '100%' }}>
+                            <Video 
+                              source={{ uri: attachment }} 
+                              style={{ width: '100%', aspectRatio: 16/9, marginBottom: 10 }} 
+                              paused={isPaused}
+                              controls={true} 
+                            />
+                            <View style={styles.videoControls}></View>
+                          </View>
+                        ) : attachment.includes('.mp3') || attachment.includes('.wav') ? (
+                          <View>
+                            {isPlaying ? (
+                              <TouchableOpacity onPress={stopAudio} style={{ backgroundColor: 'red', padding: 10, borderRadius: 5 }}>
+                                <Text style={{ color: 'white', textAlign: 'center' }}>Stop</Text>
+                              </TouchableOpacity>
+                            ) : (
+                              <TouchableOpacity onPress={() => playAudio(attachment)} style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5 }}>
+                                <Text style={{ color: 'white', textAlign: 'center', textDecorationLine: 'underline' }}>Play</Text>
+                              </TouchableOpacity>
+                            )}
+                          </View>
+
+                        ) : attachment.includes('.pdf') ? (
+                          <TouchableOpacity onPress={() => Linking.openURL(attachment)}>
+                            <View style={styles.horizontalLine}></View>
+                            <Text style={styles.selectedPostDescription}> Adjuntos:</Text>
+                            <Text style={[styles.postAttachment, { color: 'blue', textDecorationLine: 'underline' }]}>{attachment}</Text>
+                          </TouchableOpacity>
+                        ) : (
+                          <Text style={styles.postAttachment}>Attachment: {attachment}</Text>
+                        )
+                      ) : (
+                        <Text style={styles.postAttachment}>Attachment: {attachment}</Text>
+                      )}
+                    </View>                 
+                  ))}
                 </View>
               </View>
             </View>
@@ -115,9 +254,10 @@ export const CommentLog = (props: { text: string }) => {
       </View>
     </ScrollView>
   );
+
 }
 
-const styles = StyleSheet.create({
+const stylesLocal = StyleSheet.create({
   container: {
     justifyContent: 'center',
     paddingHorizontal: 20,
---
+++ my-app/app/components/styles.ts
@@ -0,0 +1,144 @@
+// styles.js
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+  },
+  container: {
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    flex: 1,
+  },
+  postDetailscontainer: {
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    flex: 1,
+  },
+  titleAndButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 20,
+    color: 'steelblue',
+    alignSelf: 'center',
+  },
+  postContainer: {
+    padding: 10,
+  },
+  postTitle: {
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  postAutorDate: {
+    marginBottom: 5,
+    color: 'gray',
+  },
+  postDescription: {
+    lineHeight: 22,
+    marginBottom: 5,
+    fontSize: 14,
+  },
+  selectedPostTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 20,
+    color: 'steelblue',
+    alignSelf: 'center',
+    flex: 1, 
+    textAlign: 'center', 
+  },
+  selectedPostAutorDate: {
+    marginBottom: 25,
+    color: 'gray',
+  },
+  selectedPostDescription: {
+    lineHeight: 22,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  pagination: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 5,
+  },
+  pageButton: {
+    fontSize: 14,
+    color: 'blue',
+  },
+  pageNumber: {
+    fontSize: 16,
+  },
+  disabled: {
+    color: 'gray',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#F7F7F7',
+  },
+  searchInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  horizontalLine: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 0.5,
+    marginVertical: 10,
+  },
+  postAttachment: {
+    color: 'blue',
+    marginBottom: 20,
+    textDecorationLine: 'underline',
+  },
+  separator: {
+    marginVertical: 10,
+    height: 1,
+    width: '100%',
+    backgroundColor: 'steelblue',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  inputComment: {
+    width: '100%',
+    height: 100,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    textAlignVertical: "top",
+  },
+  videoControls: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+});
+
+export default styles;
