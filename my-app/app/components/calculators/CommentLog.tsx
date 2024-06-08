44c4584b5e2472319790c60228eafad913abf8c6
Kvn2912 <111811080+Kvn2912@users.noreply.github.com>
2024-06-07 22:31:54 -0600

    Revert "Comentarios multimedia"

--- my-app/app/components/calculators/CommentLog.tsx
+++ my-app/app/components/calculators/CommentLog.tsx
@@ -1,140 +1,103 @@
-import { Button, Text, View, StyleSheet, ScrollView, TextInput, ActivityIndicator, TouchableOpacity, Linking } from "react-native";
+import { Button, Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
 import React, { useEffect, useState } from 'react';
 import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
-import { Controller, useForm } from 'react-hook-form';
-import { useFetchUserData } from "../../hooks/FetchData";
-import auth from "@react-native-firebase/auth";
-import * as DocumentPicker  from 'expo-document-picker';
-import storage from '@react-native-firebase/storage';
-import { Image } from 'react-native';
-import Video, {VideoRef} from 'react-native-video';
-import styles from '../styles';
-import Sound from 'react-native-sound';
-
+import { Controller, useForm, SubmitHandler } from 'react-hook-form';
+import useUserRole from '../../hooks/UserRole'; // Importa el hook para obtener el rol del usuario
+import { getUniqueUserId } from '../../hooks/UserUtils'; // Ruta al archivo UserUtils.js
 
 type Comment = {
-  id: string;
-  Attachment: string[];
+  id: string; // Agrega la propiedad id
   Name: string;
   DateTime: FirebaseFirestoreTypes.Timestamp;
   Comment: string;
+  UserId: string; // incluir el UserId en el tipo Comment
+  Response?: string; // Agrega el campo de respuesta
+}
+
+type CommentFormData = {
+  Name: string;
+  Comment: string;
+  Response?: string;
 }
 
 export const CommentLog = (props: { text: string }) => {
+  const { userRole, userId: loggedUserId } = useUserRole(); //rol y el ID del usuario
   const [comments, setComments] = useState([] as Comment[]);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const { control, handleSubmit, reset } = useForm();
-  const { userAuth, userData } = useFetchUserData();
-  const [fileUris, setFileUris] = useState<string[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [isPaused] = useState(true);
-  const [audio, setAudio] = useState<Sound | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  
+  const [showComments, setShowComments] = useState(false);
+  const [showResponse, setShowResponse] = useState<number | null>(null); // Estado para controlar la visibilidad de la respuesta
+  const [commentPosted, setCommentPosted] = useState(false); // Estado para controlar la visibilidad del mensaje
+  const { control, handleSubmit, reset } = useForm<CommentFormData>();
+  const [userId, setUserId] = useState('');
+
+  // Asignar id única a un usuario no logueado
   useEffect(() => {
-    const subscriber = firestore()
-      .collection(props.text)
-      .orderBy('DateTime', 'desc')
-      .onSnapshot((res) => {
-        const comments = [] as Comment[];
-        res.forEach((documentSnapshot) => comments.push(documentSnapshot.data() as Comment));
-        setComments(comments);
+    const fetchUserId = async () => {
+      if (userRole !== 'Administrador' && userRole !== 'Docente') {
+        const id = await getUniqueUserId();
+        setUserId(id);
+      } else {
+        setUserId(''); // Asignar una cadena vacía para los administradores y docentes
+      }
+    };
+  
+    fetchUserId();
+  }, [userRole]);
+
+  useEffect(() => {
+    const subscriber = firestore().collection(props.text).onSnapshot((res) => {
+      const comments = [] as Comment[];
+      res.forEach((documentSnapshot) => {
+        const commentData = documentSnapshot.data() as Comment;
+        commentData.id = documentSnapshot.id;
+        comments.push(commentData);
       });
+      setComments(comments);
+    });
 
     return () => subscriber();
   }, []);
 
-  const pickFile = async () => {
+  const addComment: SubmitHandler<CommentFormData> = async (data) => {
     try {
-      const documentResult = await DocumentPicker.getDocumentAsync();
-      const urifile = documentResult.assets![0];
-
-
-        const fileUri = urifile.uri;
-        const fileName = urifile.name;
-        setFileUris(prevFileUris => [...prevFileUris, fileUri]);
-
-
-    } catch (err) {
-      console.error("Error picking file: ", err);
-    }
-  };
-
-  const uploadFiles = async (uris: string[], postId: string) => {
-    try {
-      const uploadTasks = uris.map(async (uri) => {
-        const filename = uri.substring(uri.lastIndexOf('/') + 1);
-        const uploadUri = uri.replace('file://', '');
-        const task = storage().ref(filename).putFile(uploadUri);
-        console.log(uploadUri);
-        await task;
-        return storage().ref(filename).getDownloadURL();
-      });
-
-      const urls = await Promise.all(uploadTasks);
-
-      await firestore().collection(props.text).doc(postId).update({
-        Attachment: urls,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const addComment = async (data: any) => {
-    setIsSubmitting(true);
-    try {
-      const postRef = await firestore().collection(props.text).add({
-        Attachment: [],
+      await firestore().collection(props.text).add({
         Name: data.Name,
         DateTime: firestore.Timestamp.fromDate(new Date()),
-        Comment: data.Comment
+        Comment: data.Comment,
+        UserId: userRole ? loggedUserId : userId  // Agrega el ID del usuario al comentario
       });
       reset();
-
-      if (fileUris.length > 0) {
-        await uploadFiles(fileUris, postRef.id);
-        setFileUris([]);
-      }
-
+      setCommentPosted(true);
+      setTimeout(() => setCommentPosted(false), 3000); // Ocultar el mensaje después de 3 segundos
     } catch (error) {
       console.error("Error agregando el comentario: ", error);
-    } finally {
-      setIsSubmitting(false);
     }
   };
 
-  const playAudio = (uri: string) => {
-    if (audio) {
-      audio.stop(() => {
-        audio.release();
-      });
-    }
-  
-    const sound = new Sound(uri, Sound.MAIN_BUNDLE, (error) => {
-      if (error) {
-        console.log('failed to load the sound', error);
+  const addResponse = async (data: { Response: string }, commentId: string) => {
+    try {
+      if (!data.Response) {
+        console.error("La respuesta no puede estar vacía.");
         return;
       }
-      setAudio(sound);
-      sound.play(() => {
-        sound.release();
-        setIsPlaying(false);
+      
+      await firestore().collection(props.text).doc(commentId).update({
+        Response: data.Response
       });
-    });
-    setIsPlaying(true);
-  };
-  
-  const stopAudio = () => {
-    if (audio) {
-      audio.stop(() => {
-        audio.release();
-        setAudio(null);
-      });
-      setIsPlaying(false);
+      reset();
+    } catch (error) {
+      console.error("Error agregando la respuesta: ", error);
     }
   };
-  
+
+  const filteredComments = comments
+  .slice()
+  .sort((a, b) => b.DateTime.toMillis() - a.DateTime.toMillis())
+  .filter(comment => {
+    if (userRole === 'Administrador' || userRole === 'Docente') {
+      return true; // Admins pueden ver todos los comentarios
+    }
+    return comment.UserId === (userRole ? loggedUserId : userId);
+  });
 
   return (
     <ScrollView
@@ -143,124 +106,116 @@ export const CommentLog = (props: { text: string }) => {
     >
       <View style={styles.container}>
         <View style={{ alignItems: 'flex-start' }}>
+          <Button
+            title={showComments ? "Ocultar comentarios" : "Mostrar comentarios"}
+            onPress={() => setShowComments(!showComments)}
+          />
         </View>
-        <View>
-          <View style={styles.separator} />
-          {userAuth && userData && (
-            <>
-              <Text style={styles.title}>Nuevo comentario</Text>
-              <View>
-                <Controller
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      placeholder="Nombre"
-                      style={styles.input}
-                      onBlur={onBlur}
-                      onChangeText={onChange}
-                      value={auth().currentUser?.displayName || ""}
-                      keyboardType="default"
-                      editable={false}
-                    />
-                  )}
-                  name="Name"
-                  defaultValue={auth().currentUser?.displayName || ""}
-                />
-                <Controller
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      placeholder="Comentario"
-                      style={styles.inputComment}
-                      onBlur={onBlur}
-                      onChangeText={onChange}
-                      value={value}
-                      keyboardType="default"
-                      multiline={true}
-                    />
-                  )}
-                  name="Comment"
-                />
-                <Button title="Elegir Archivo" onPress={pickFile} />
-                {fileUris && <Text>Archivo seleccionado: {fileUris.join(', ')}</Text>}
-                {loading ? (
-                  <ActivityIndicator size="large" color="#0000ff" />
-                ) : (
-                  <Button onPress={handleSubmit(addComment)} title="Enviar" />
+        {showComments && (
+          <View>
+            <View style={styles.separator} />
+            <Text style={styles.title}>Nuevo comentario</Text>
+            <View>
+              <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    placeholder="Nombre"
+                    style={styles.input}
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    keyboardType="default"
+                  />
                 )}
-                {isSubmitting && <ActivityIndicator size="large" color="steelblue" />}
-              </View>
-            </>
-          )}
-          <Text style={styles.title}>Comentarios</Text>
-          {comments.map((comment, index) => (
-            <View key={index}>
-              <View style={stylesLocal.commentBox} key={index}>
-                <View style={stylesLocal.commentContainer}>
-                  <Text style={stylesLocal.commentName}>{comment.Name}</Text>
-                  <Text style={stylesLocal.commentDateTime}>{new Date(comment.DateTime.toDate()).toLocaleString()}</Text>
-                  <Text style={stylesLocal.commentText}>{comment.Comment}</Text>
-                  {Array.isArray(comment.Attachment) && comment.Attachment.map((attachment, attachmentIndex) => (
-                    <View key={attachmentIndex} style={{ maxWidth: '100%', marginBottom: 10 }}>
-                      {typeof attachment === 'string' && attachment.startsWith('http') ? (
-                        attachment.includes('.jpg') ? (
-                          <Image 
-                            source={{ uri: attachment }} 
-                            style={{ width: '100%', aspectRatio: 1, marginBottom: 10 }} 
-                            resizeMode="contain"
-                          />
-                        ) : attachment.includes('.mp4') ? (
-                          <View style={{ maxWidth: '100%' }}>
-                            <Video 
-                              source={{ uri: attachment }} 
-                              style={{ width: '100%', aspectRatio: 16/9, marginBottom: 10 }} 
-                              paused={isPaused}
-                              controls={true} 
-                            />
-                            <View style={styles.videoControls}></View>
-                          </View>
-                        ) : attachment.includes('.mp3') || attachment.includes('.wav') ? (
-                          <View>
-                            {isPlaying ? (
-                              <TouchableOpacity onPress={stopAudio} style={{ backgroundColor: 'red', padding: 10, borderRadius: 5 }}>
-                                <Text style={{ color: 'white', textAlign: 'center' }}>Stop</Text>
-                              </TouchableOpacity>
-                            ) : (
-                              <TouchableOpacity onPress={() => playAudio(attachment)} style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5 }}>
-                                <Text style={{ color: 'white', textAlign: 'center', textDecorationLine: 'underline' }}>Play</Text>
-                              </TouchableOpacity>
-                            )}
-                          </View>
-
-                        ) : attachment.includes('.pdf') ? (
-                          <TouchableOpacity onPress={() => Linking.openURL(attachment)}>
-                            <View style={styles.horizontalLine}></View>
-                            <Text style={styles.selectedPostDescription}> Adjuntos:</Text>
-                            <Text style={[styles.postAttachment, { color: 'blue', textDecorationLine: 'underline' }]}>{attachment}</Text>
-                          </TouchableOpacity>
-                        ) : (
-                          <Text style={styles.postAttachment}>Attachment: {attachment}</Text>
-                        )
-                      ) : (
-                        <Text style={styles.postAttachment}>Attachment: {attachment}</Text>
-                      )}
-                    </View>                 
-                  ))}
-                </View>
-              </View>
+                name="Name"
+              />
+              <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    placeholder="Comentario"
+                    style={styles.inputComment}
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    keyboardType="default"
+                    multiline={true}
+                  />
+                )}
+                name="Comment"
+              />
+              <Button onPress={handleSubmit(addComment)} title="Enviar" />
+              {commentPosted && <Text style={styles.successMessage}>Comentario publicado</Text>}
             </View>
-          ))}
-        </View>
+            <Text style={styles.title}>Comentarios</Text>
+            {filteredComments.length === 0 ? (
+              <Text style={styles.commentText}>Sin comentarios</Text>
+            ) : (
+              filteredComments.map((comment, index) => (
+                <View key={comment.id}>
+                  {comment.Name && comment.DateTime && comment.Comment ? (
+                    <View style={styles.commentBox} key={comment.id}>
+                      <View style={styles.commentContainer}>
+                        <Text style={styles.commentName}>Nombre: {comment.Name}</Text>
+                        <Text style={styles.commentDateTime}>Fecha y hora: {new Date(comment.DateTime.toDate()).toLocaleString()}</Text>
+                        <Text style={styles.commentText}>Comentario: {comment.Comment}</Text>
+                      </View>
+                      {comment.Response ? (
+                        <View style={styles.commentContainer}>
+                          <Text style={styles.title}>Respuestas</Text>
+                          <Text style={styles.commentResponse}>Respuesta: {comment.Response}</Text>
+                        </View>
+                      ) : (
+                        <View style={styles.commentContainer}>
+                          <Text style={styles.commentResponse}>
+                            {(userRole === 'Administrador' || userRole === 'Docente') && (
+                              <Button onPress={() => setShowResponse(index)} title="Responder" />
+                            )}
+                          </Text>
+                          {showResponse === index && (
+                            <View>
+                              <Controller
+                                control={control}
+                                render={({ field: { onChange, onBlur, value } }) => (
+                                  <TextInput
+                                    placeholder="Respuesta"
+                                    style={styles.input}
+                                    onBlur={onBlur}
+                                    onChangeText={onChange}
+                                    value={value}
+                                    keyboardType="default"
+                                  />
+                                )}
+                                name="Response"
+                              />
+                              <Button onPress={handleSubmit((data) => {
+                                if (data.Response) {
+                                  addResponse({ Response: data.Response }, comment.id)
+                                } else {
+                                  console.error("El valor de Response está indefinido.");
+                                }
+                              })} title="Enviar respuesta" />
+                            </View>
+                          )}
+                        </View>
+                      )}
+                    </View>
+                  ) : null}
+                </View>
+              ))
+            )}
+          </View>
+        )}
       </View>
     </ScrollView>
   );
-
 }
 
-const stylesLocal = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
     paddingVertical: 10,
   },
   title: {
@@ -291,31 +246,39 @@ const stylesLocal = StyleSheet.create({
   },
   commentDateTime: {
     fontStyle: 'italic',
-    marginBottom: 5,
-    color: 'gray',
   },
   commentText: {
-    lineHeight: 20,
+    marginBottom: 10,
+  },
+  commentResponse: {
+    marginTop: 10,
+    color: 'green', // Agregar estilo para el color verde
   },
   input: {
-    width: '100%',
     height: 40,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    backgroundColor: '#fff',
+    borderColor: 'gray',
     borderWidth: 1,
-    borderColor: '#ccc',
+    marginBottom: 10,
+    paddingLeft: 10,
     borderRadius: 5,
   },
   inputComment: {
-    width: '100%',
-    height: 200,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    backgroundColor: '#fff',
+    height: 80,
+    borderColor: 'gray',
     borderWidth: 1,
-    borderColor: '#ccc',
+    marginBottom: 10,
+    paddingLeft: 10,
     borderRadius: 5,
-    textAlignVertical: "top"
+  },
+  successMessage: {
+    color: 'green',
+    marginTop: 10,
   },
 });
+
+
+
+
+
+
+
