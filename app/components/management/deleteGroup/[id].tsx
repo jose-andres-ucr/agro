d8c1398cec56641c8c56d81008e5d4d933cb089e
Randy <randy.robles@ucr.ac.cr>
2024-07-09 16:10:01 -0600

    Avance borrar grupos

--- app/components/management/deleteGroup/[id].tsx
+++ app/components/management/deleteGroup/[id].tsx
@@ -1,16 +1,175 @@
-import { useLocalSearchParams } from "expo-router";
-import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useContext, useEffect, useState } from "react";
+import { View, Text, Modal, StyleSheet, TouchableOpacity } from "react-native";
+import { UserContext } from "../../../hooks/context/UserContext";
+import { useFetchGroups } from "../../../hooks/useFetchData";
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
+import { Button } from 'react-native-paper';
 
 export default function ManageStudents() {
+  const { userData } = useContext(UserContext);
+  const groups = useFetchGroups(userData?.Role, userData?.Email);
   const { id } = useLocalSearchParams();
+  const [loading, setLoading] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     console.log("ID del grupo:", id);
   }, [id]);
+
+  const deleteGroup = async () => {
+    if (typeof id !== 'string') {
+      console.error('ID de grupo inválido.');
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const groupRef = firestore().collection('Groups').doc(id);
+
+      const eduGroupMaterialSnapshot = await groupRef.collection('EduGroupMaterial').get();
+
+      const deletePromises: Promise<void>[] = [];
+
+      eduGroupMaterialSnapshot.forEach(async (doc) => {
+        const data = doc.data();
+        if (data.Attachment && data.Attachment.length > 0) {
+          for (const attachmentUrl of data.Attachment) {
+            const storageRef = storage().refFromURL(attachmentUrl);
+            deletePromises.push(storageRef.delete());
+          }
+        }
+        deletePromises.push(doc.ref.delete());
+      });
+      await Promise.all(deletePromises);
+
+      await groupRef.delete();
+
+      router.back();
+      router.back();
+    } catch (error) {
+      console.error("Error eliminando el grupo: ", error);
+    } finally {
+      setLoading(false);
+      setModalVisible(false);
+    }
+  };
+
+  const confirmDelete = () => {
+    setModalVisible(true);
+  };
+
   return (
-    <View>
-      <Text>Eliminar el grupo {id}</Text>
+    <View style={styles.container}>
+      <Text
+        style={{ fontSize: 20, textAlign: "center", marginBottom: 20}}
+      >
+        ¿Eliminar el grupo {id}?
+      </Text>
+      <Button
+        mode="contained"
+        buttonColor="red"
+        style={{
+          width: "50%",
+          alignSelf: "center",
+          marginVertical: 30,
+          marginRight: 20,
+        }}
+        labelStyle={{ fontSize: 16 }}
+        onPress={confirmDelete}
+        disabled={loading}
+      >
+        Eliminar Grupo
+      </Button>
+      
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>¿Estás seguro de que deseas eliminar este grupo?</Text>
+            <View style={styles.buttonContainer}>
+              <Button
+                mode="contained"
+                buttonColor="red"
+                style={{
+                  width: "50%",
+                  alignSelf: "flex-end",
+                  marginVertical: 30,
+                  marginRight: 20,
+                }}
+                labelStyle={{ fontSize: 16 }}
+                onPress={deleteGroup}
+              >
+                Sí
+              </Button>
+              <TouchableOpacity 
+                style={[styles.button, styles.buttonCancel]}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: 300,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 20,
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    minWidth: 80,
+    alignItems: 'center',
+  },
+  buttonCancel: {
+    backgroundColor: '#cccccc',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
